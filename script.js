875fec5b913751af3c0fa01cd90ad4e160536a10
SohilaEssam2210 <essamsohila62@gmail.com>
2024-08-23 03:13:51 +0300

    v1

---
+++ script.js
@@ -0,0 +1,73 @@
+// Filters
+let saturate = document.getElementById("saturate");
+let contrast = document.getElementById("contrast");
+let brightness = document.getElementById("brightness");
+let sepia = document.getElementById("sepia");
+let grayscale = document.getElementById("grayscale");
+let blur = document.getElementById("blur");
+let hueRotate = document.getElementById("hue-rotate");
+
+//btns
+let upload = document.getElementById("upload");
+let download = document.getElementById("download");
+let reset = document.getElementById("reset");
+
+let imgBox = document.querySelector(".img-box");
+let img = document.getElementById("img");
+
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+window.onload = () => {
+  download.style.display = "none";
+  reset.style.display = "none";
+  imgBox.style.display = "none";
+};
+
+function resetValue() {
+  img.style.filter = "none";
+  saturate.value = "100";
+  contrast.value = "100";
+  brightness.value = "100";
+  sepia.value = "0";
+  grayscale.value = "0";
+  blur.value = "0";
+  hueRotate.value = "0";
+}
+
+upload.onchange = () => {
+  resetValue();
+  download.style.display = "block";
+  reset.style.display = "block";
+  imgBox.style.display = "block";
+  let file = new FileReader();
+  file.readAsDataURL(upload.files[0]);
+  file.onload = () => {
+    img.src = file.result;
+  };
+  //when image is loaded Drow the canvas
+  img.onload = () => {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    img.style.display = "none";
+  };
+};
+
+let filters = document.querySelectorAll("ul li input");
+filters.forEach((filter) => {
+  filter.addEventListener("input", () => {
+    ctx.filter = `
+    saturate(${saturate.value}%) 
+    contrast(${contrast.value}%)
+    brightness(${brightness.value}%) 
+    sepia(${sepia.value}%)
+    grayscale(${grayscale.value}) 
+    blur(${blur.value}px)
+    hue-rotate(${hueRotate.value}deg)`;
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+  });
+});
+
+download.onclick = () => {
+  download.href = canvas.toDataURL();
+};
